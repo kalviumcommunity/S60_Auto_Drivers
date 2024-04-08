d99d54d8fc3fb17424006912f5fc55745efc507f
vinnugollakoti <vinnugollakoti289@gmail.com>
2024-04-08 12:25:23 +0530

    added database

--- server/server.js
+++ server/server.js
@@ -1,14 +1,30 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const cors = require("cors")
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-const app = express()
-const port = 3001
+const app = express();
+const port = 3001;
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/ping", (req, res) => {
-    res.send("pong")
-})
+    res.send("pong");
+});
 
-app.listen(port, () => {
-    console.log("server is running on 3001")
+// MongoDB Connection
+mongoose.connect("mongodb+srv://vinnugollakoti:123@cluster0.cwivpr4.mongodb.net/mydatabase?retryWrites=true&w=majority")
+.then(() => {
+    console.log("MongoDB connected successfully");
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
 })
+.catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+});
+
+app.use((err, res) => {
+    console.error(err.stack);
+    res.status(500).send("Something went wrong!");
+});
