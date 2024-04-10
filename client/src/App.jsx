d2fef18d4ae9937747d363b2552661747e08616b
vinnugollakoti <vinnugollakoti289@gmail.com>
2024-04-10 12:05:06 +0530

    updated reactfile

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,13 @@
-import {BroweserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Components/Home'
 const App = () => {
   return (
-    <BroweserRouter>
+    <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home /> }/>
         <Route path='*' element={<h1>Error 404</h1>} />
       </Routes>
-    </BroweserRouter>
+    </BrowserRouter>
   )
 }
 
